29392476b3b84aecc003b035d43d5aca25823a53
Santosh Kumar Saravanan <santosh.saravanan24@imperial.ac.uk>
2024-12-12 13:06:50 +0000

    Fix deprecated param

--- components/recipe.py
+++ components/recipe.py
@@ -4,15 +4,8 @@ def render_recipe_section(recipe):
     st.subheader("Recipe")
     
     col1, col2 = st.columns([1, 1])
-    
+
     with col1:
-        st.image(
-            "https://images.unsplash.com/photo-1588166524941-3bf61a9c41db?w=800&h=600&fit=crop",
-            caption="Chicken Tikka Masala",
-            use_container_width=True
-        )
-    
-    with col2:
         # Ingredient List Card
         st.markdown("### Ingredients")
         for ingredient in recipe['ingredients']:
@@ -22,3 +15,11 @@ def render_recipe_section(recipe):
         st.markdown("### Instructions")
         for step in recipe['steps']:
             st.markdown(f"{step['step']}. {step['instruction']}")
+    
+    with col2:
+        st.image(
+            "https://images.unsplash.com/photo-1588166524941-3bf61a9c41db?w=800&h=600&fit=crop",
+            caption="Chicken Tikka Masala",
+            use_container_width=True
+        )
+    
